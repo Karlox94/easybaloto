2e82fa9a8189f92894e4424944e9a9a2eb1fac99
karlox94 <ikarloxi@gmail.com>
2015-04-30 20:37:34 -0500

    vistas terminadas (creo XD)

--- numeros.js
+++ numeros.js
@@ -4,10 +4,10 @@ function lol(){
 
 for (var i = 1;  i <= 45; i++) {
 
-$('select').append($('<option>', {
+$('select').append($('<option value="'i'">', {
     value: i,
     text: i
-	}));
+	}'</option>'));
 }
 	
 };
